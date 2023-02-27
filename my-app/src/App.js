3f8af42c4bb86318206899b48fb8de19d4cb0f7c
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-02-27 21:22:12 +0000

    Added styles to hero main page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,10 @@ function App() {
 
   return (
     <div className="App">
+        <div className="hero">
+          <h1>Ben Allington</h1>
+          <p>benallington1995@gmail.com</p>
+        </div>
         <Register/>
     </div>
   );
